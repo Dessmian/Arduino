77f8968631a927b9d86735723ce581cd957971ab
Demian Murray <47385734+Dessmian@users.noreply.github.com>
2019-04-10 12:23:47 -0300

    Add files via upload

---
+++ sketch_apr10c.ino
@@ -0,0 +1,32 @@
+#define P HIGH
+#define A LOW
+void autoIncreible ( int [] ) ;
+void setup() {
+    int leds [] = { 1 , 2 , 3 , 4 , 5 } ;
+    int i ;
+    for ( i = 0 ; i < 5 ; i++ ) {
+      pinMode ( leds [i] , OUTPUT ) ;
+    }
+}
+void loop() {
+
+  int leds [] = { 1 , 2 , 3 , 4 , 5 } ;
+  autoIncreible (leds) ;  
+}
+
+// usar valor absoluto
+void autoIncreible ( int pins []){
+  int tiempo = 15 ;
+  int i ;
+  for ( i = 0 ; i < 5 ; i ++ ){
+    digitalWrite ( pins [ i ] , P ) ;
+    delay ( tiempo ) ;
+    digitalWrite ( pins [ i ] , A ) ;
+  }
+  for ( i = 3 ; i > 0 ; i-- ){
+    digitalWrite ( pins [ i ] , P ) ;
+    delay ( tiempo ) ;
+    digitalWrite ( pins [ i ] , A ) ;
+  } 
+}
+
