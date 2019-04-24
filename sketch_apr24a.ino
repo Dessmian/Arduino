ace32f75a31041693f094b5c24928866b0a0b420
Demian Murray <47385734+Dessmian@users.noreply.github.com>
2019-04-24 12:17:16 -0300

    Add files via upload

---
+++ sketch_apr24a.ino
@@ -0,0 +1,34 @@
+#define LED 5
+int estadoBoton ;
+int estadoAnteriorBoton = 1 ;
+int estadoLed = 0 ;
+int iteration = 0 ;
+void setup() {
+  pinMode ( 2 , INPUT ) ;
+  pinMode ( LED , OUTPUT ) ;
+  Serial.begin ( 9600 ) ;
+}
+
+void loop() {
+
+  estadoBoton = digitalRead ( 2 ) ;
+  if ( estadoBoton != estadoAnteriorBoton ) {
+    if ( estadoBoton == HIGH ) {
+      estadoLed = !estadoLed ;
+      delay ( 20 ) ;
+      digitalWrite ( LED , estadoLed ) ;
+      if ( estadoLed ) {
+        Serial.println ( "Prendido" ) ;
+      } else {
+        Serial.println ( "Apagado" ) ;
+      }
+    }  
+  }
+  estadoAnteriorBoton = estadoBoton ;
+  delay ( 150 ) ;
+}
+int buttonSwitch ( int buttonPin )
+{
+   
+}
+
